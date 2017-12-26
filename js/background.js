6cb079e0e78a3144e9e9c0dd14c24203bd40b79c
Mukil Kesavan <mukilk7@gmail.com>
2017-12-26 10:25:09 -0800

    Adding ability to block only specific URLs

--- js/background.js
+++ js/background.js
@@ -1,28 +1,74 @@
 var blockedSites = [];
+var blockedUrls = [];
 var tabBlockingMap = {};
 
-chrome.storage.local.get("blocked", function(items) {
-  if (items.blocked)
-    blockedSites = items.blocked;
+chrome.storage.local.get("blockedSites", function(items) {
+  if (items.blockedSites)
+    blockedSites = items.blockedSites;
 });
 
-function addBlockedSite(tabid, blockedSite) {
-  blockedSites.push(blockedSite);
-  tabBlockingMap[tabid] = blockedSite;
+chrome.storage.local.get("blockedUrls", function(items) {
+  if (items.blockedUrls)
+    blockedUrls = items.blockedUrls;
+});
+
+function addBlockedSiteOrUrl(tabid, blocked, isSite) {
+  if (isSite) {
+    blockedSites.push(blocked);
+  } else {
+    blockedUrls.push(blocked);
+  }
+  tabBlockingMap[tabid] = blocked;
 }
 
-function unlistSite(tabid, site) {
-  var i = blockedSites.indexOf(site);
-  if (i > -1)
-    blockedSites.splice(i, 1);
-  chrome.storage.local.set( {blocked: blockedSites}, function() {
-    console.log("Site Unlisted");
+function extract_url_domain(data) {
+  var a = document.createElement('a');
+  a.href = data;
+  return a.hostname;
+}
+
+function findInList(list, urlstr, partialMatch) {
+  var matched_indices = [];
+  if (!partialMatch) {
+    var i = list.indexOf(urlstr);
+    if (i >= 0) {
+      matched_indices.push(i);
+    }
+    return matched_indices;
+  }
+  var rootUrl = extract_url_domain(urlstr);
+  for (var i = 0; i < list.length; ++i) {
+    if (list[i].match(rootUrl)) {
+      matched_indices.push(i);
+    }
+  }
+  return matched_indices;
+}
+
+function unlist(blockedList, curUrl, blockedType, isSite) {
+  var matched_indices = findInList(blockedList, curUrl, isSite);
+  console.log("found " + matched_indices.length + " matches for " + curUrl);
+  for (var i = matched_indices.length - 1; i >= 0; --i) {
+    blockedList.splice(matched_indices[i], 1);
+  }
+  var storedict = {};
+  storedict[blockedType] = blockedList;
+  chrome.storage.local.set(storedict, function() {
+    console.log("Site or url(s) Unlisted");
   });
+}
+
+function unlistSiteOrUrl(tabid, curUrl, isSite) {
+  //we only have to partial match urls if user clicked remove site
+  unlist(blockedUrls, curUrl, "blockedUrls", isSite);
+  //we never have to partial match while checking blocked sites
+  unlist(blockedSites, curUrl, "blockedSites", false);
   tabBlockingMap[tabid] = 0;
 }
 
 function clearBlacklist() {
   blockedSites = [];
+  blockedUrls = [];
   tabBlockingMap = {};
 }
 
@@ -40,6 +86,15 @@ function requestChecker(request) {
         if (request.url.match(new RegExp(
             ".*" + blockedSites[i] + ".*", "i"))) {
           tabBlockingState = blockedSites[i];
+          console.log("found " + request.url + " in blocked sites");
+        }
+      }
+      if (tabBlockingState == 0) {
+        for (var i = 0; i < blockedUrls.length; ++i) {
+         if (request.url.toLowerCase() == blockedUrls[i].toLowerCase()) {
+          tabBlockingState = blockedUrls[i];
+          console.log("found " + request.url + " in blocked urls");
+         }
         }
       }
       chrome.tabs.getSelected(null, function(tab) {
--- js/blockedSite.js
+++ js/blockedSite.js
@@ -4,24 +4,23 @@ site = site.substring(site.indexOf("=") + 1);
 $("#blockMessage").text(site + " has been Blacklisted.");
 
 var content = $("#countdown");
-var i = 15;
+var UNLOCK_INTERVAL = 15;
 var interval = 0;
 
-function updateCountdown() {
-  content.text("Unlisting " + site + " in " + --i + " seconds...");
-  if (i == 0) {
-    clearInterval(interval);
-    chrome.tabs.getCurrent(function(tab) {
-      chrome.extension.getBackgroundPage().unlistSite(tab.id, site);
-    });
-    window.location = site;
-  }
-}
-
-function beginCountdown() {
-  i = 15;
-  content.text("Unlisting " + site + " in " + i + " seconds...");
-  interval = setInterval(function() {updateCountdown(i)}, 1000);
+function beginCountdown(isSite) {
+  var i = UNLOCK_INTERVAL;
+  var unlistType = (isSite)? "entire site": "url";
+  content.text("Unlisting " + unlistType + " in " + i + " seconds...");
+  interval = setInterval(function() {
+    content.text("Unlisting " + unlistType + " in " + --i + " seconds...");
+    if (i == 0) {
+      clearInterval(interval);
+      chrome.tabs.getCurrent(function(tab) {
+        chrome.extension.getBackgroundPage().unlistSiteOrUrl(tab.id, site, isSite);
+      });
+      window.location = site;
+    }
+  }, 1000);
 }
 
 function modalHidden() {
@@ -36,12 +35,13 @@ $("#unlistModal").on('hidden', modalHidden);
 $("#cancelUnlist").click(hideModal);
 
 chrome.extension.onMessage.addListener(
-    function(message, sender, sendResponse) {
-  chrome.tabs.getCurrent(function(tab) {
-    if (tab.id == message) {
-      $("#unlistModal").modal("show");
-      beginCountdown();
-    }
-  });
-});
+  function(request, sender, sendResponse) {
+    chrome.tabs.getCurrent(function(tab) {
+      if (tab.id == request.tabid) {
+        $("#unlistModal").modal("show");
+        beginCountdown(request.isSite);
+      }
+    });
+  }
+);
 
